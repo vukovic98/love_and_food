810e01ce86332fc8398f7c8f21dc496a1fb3fea4
vukovic98 <vladimirvukovic98@hotmail.rs>
2021-06-15 09:13:06 +0200

    404-PageNotFound, minor design changes

--- app-front/love-and-food-fe/src/app/app-routing.module.ts
+++ app-front/love-and-food-fe/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from "./components/shared/not-found/not-found.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
@@ -19,6 +20,7 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
   },
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
