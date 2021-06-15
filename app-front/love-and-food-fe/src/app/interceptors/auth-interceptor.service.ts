15dbfeccfd0eefbedc4a42f73bb22860f125d0ed
vukovic98 <vladimirvukovic98@hotmail.rs>
2021-06-15 12:52:27 +0200

    JWT expiration handling

--- app-front/love-and-food-fe/src/app/interceptors/auth-interceptor.service.ts
+++ app-front/love-and-food-fe/src/app/interceptors/auth-interceptor.service.ts
@@ -1,9 +1,39 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import Swal from 'sweetalert2';
+import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    if (err.status === 401 || err.status === 403) {
+
+      this.authService.logout();
+
+      this.router.navigate(['/auth/login']);
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Your token has expired. Please login again.',
+        showConfirmButton: false,
+        timer: 3000
+      })
+
+      return of(err.message);
+    }
+    return throwError(err);
+  }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const jwtToken = localStorage.getItem('accessToken');
         if (!jwtToken) {
@@ -16,7 +46,7 @@ export class AuthInterceptorService implements HttpInterceptor {
         const modifiedReq = req.clone({
             headers: req.headers.set('Authorization', 'Bearer ' +  jwtToken)
           });
-        return next.handle(modifiedReq);
+        return next.handle(modifiedReq).pipe(catchError(x=> this.handleAuthError(x)));
     }
 
 }
